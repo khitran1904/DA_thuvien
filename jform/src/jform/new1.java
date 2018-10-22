57e446ad370ce33cb5b075f184cc10a67813b946
damho <damho@DESKTOP-MF388JD>
2018-10-22 23:14:32 +0700

    lol

---
+++ jform/src/jform/new1.java
@@ -0,0 +1,42 @@
+/*
+ * Created by JFormDesigner on Mon Oct 22 23:13:25 ICT 2018
+ */
+
+package jform;
+
+import java.awt.*;
+import javax.swing.*;
+import net.miginfocom.swing.*;
+
+/**
+ * @author tr?n th? kh?i
+ */
+public class new1 extends JFrame {
+	public new1() {
+		initComponents();
+	}
+
+	private void initComponents() {
+		// JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+		// Generated using JFormDesigner Evaluation license - tr?n th? kh?i
+
+		//======== this ========
+		Container contentPane = getContentPane();
+		contentPane.setLayout(new MigLayout(
+			"hidemode 3",
+			// columns
+			"[fill]" +
+			"[fill]",
+			// rows
+			"[]" +
+			"[]" +
+			"[]"));
+		pack();
+		setLocationRelativeTo(getOwner());
+		// JFormDesigner - End of component initialization  //GEN-END:initComponents
+	}
+
+	// JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+	// Generated using JFormDesigner Evaluation license - tr?n th? kh?i
+	// JFormDesigner - End of variables declaration  //GEN-END:variables
+}
